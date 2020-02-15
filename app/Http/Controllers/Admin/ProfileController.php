f062404551f4d157084a788b0f1a4559464939a6
techboost2 <jwtja6wv@gmail.com>
2020-02-15 09:02:49 +0000

    first commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -30,14 +30,55 @@ class ProfileController extends Controller
 
         return redirect('admin/profile/create');
     }
+    
+     public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('name', $cond_title)->get();
+      } else {
+          // それ以外はすべてのプロフィールを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+   }
 
-    public function edit()
+
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
 
     public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        //Validationをかける
+        $this->validate($request, Profile::$rules);
+        //Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        //送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        
+        //該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile');
     }
+    
+    // 以下を追記　　
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+  }  
+
+  
 }
